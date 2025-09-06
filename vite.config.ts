ebae55dee2c82d0e57ba992b0dc780b9ad1f996b
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2025-09-06 17:40:21 -0400

    patch: fix errors and add wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
+import {cloudflare} from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -10,7 +11,8 @@ export default ({ mode }: { mode: string }) => {
   return defineConfig({
     plugins: [
       react(),
-      tailwindcss()
+      tailwindcss(),
+      cloudflare(),
     ],
     resolve: {
       alias: {
